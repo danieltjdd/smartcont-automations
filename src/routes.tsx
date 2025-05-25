77d19d48a929b7fe6e8cbce834a6777d8d5dde3a
Seu Nome <seu.email@exemplo.com>
2025-05-25 15:49:12 -0300

    fix: remove AdminDashboard route

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,69 +1,18 @@
-import { Routes, Route } from 'react-router-dom';
-import { PrivateRoute } from '@/components/PrivateRoute';
-import Index from './pages/Index';
-import Login from './pages/Login';
-import Dashboard from './pages/Dashboard';
-import Historico from './pages/Historico';
-import Perfil from './pages/Perfil';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
 import Modulos from './pages/Modulos';
 import Solucoes from './pages/Solucoes';
-import AdminDashboard from './pages/AdminDashboard';
 import NotFound from './pages/NotFound';
 
 export function AppRoutes() {
   return (
-    <Routes>
-      <Route path="/" element={<Index />} />
-      <Route path="/login" element={<Login />} />
-      <Route
-        path="/dashboard"
-        element={
-          <PrivateRoute>
-            <Dashboard />
-          </PrivateRoute>
-        }
-      />
-      <Route
-        path="/dashboard/historico"
-        element={
-          <PrivateRoute>
-            <Historico />
-          </PrivateRoute>
-        }
-      />
-      <Route
-        path="/dashboard/perfil"
-        element={
-          <PrivateRoute>
-            <Perfil />
-          </PrivateRoute>
-        }
-      />
-      <Route
-        path="/modulos"
-        element={
-          <PrivateRoute>
-            <Modulos />
-          </PrivateRoute>
-        }
-      />
-      <Route
-        path="/solucoes"
-        element={
-          <PrivateRoute>
-            <Solucoes />
-          </PrivateRoute>
-        }
-      />
-      <Route
-        path="/admin"
-        element={
-          <PrivateRoute>
-            <AdminDashboard />
-          </PrivateRoute>
-        }
-      />
-      <Route path="*" element={<NotFound />} />
-    </Routes>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/modulos" element={<Modulos />} />
+        <Route path="/solucoes" element={<Solucoes />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
   );
 } 
